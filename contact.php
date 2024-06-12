f4c9c101426f5709b6d8aebffe41d03845adde05
Menno Worp <menno.worp@windesheim.nl>
2024-06-12 13:40:36 +0200

    Contact into PHP

--- contact.php
+++ contact.php
@@ -16,9 +16,36 @@
     </header>
 
     <main> 
+
+    <div class="container">
+  <form action="action_page.php">
+
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+
+  </form>
+  
+</div>
     <h2>Contactpagina</h2>
     <p>Klik op de link hieronder om een e-mail te sturen.</p>
     <a href="mailto:mennoworp@hotmail.com">E-mail sturen</a><br>
+
+
     </main> 
 
     <footer><hr>
